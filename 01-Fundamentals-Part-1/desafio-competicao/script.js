15116f7ebd3338ee64cbd3b6ef6834b42df30101
thales <thalesdliscano@gmail.com>
2024-01-03 09:18:58 -0400

    add-desafio-competicao-completo

--- 01-Fundamentals-Part-1/desafio-competicao/script.js
+++ 01-Fundamentals-Part-1/desafio-competicao/script.js
@@ -1,3 +1,16 @@
+document.getElementById("btn-media").addEventListener("click", function () {
+  setTimeout(() => {
+    window.location.href = window.location.href;
+  }, 5000);
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  setTimeout(() => {
+    window.location.href = window.location.href;
+  }, 5000);
+});
+
 function fazerMedia() {
   let p1Gol, p2Gol, p3Gol;
   p1Gol = document.getElementById("pontuacaoUm-golfinhos").value;
@@ -15,13 +28,41 @@ function fazerMedia() {
 
   // if (pontuacaoContaGol >= 100 && pontuacaoContaCoa >= 100) {
   if (pontuacaoContaGol > pontuacaoContaCoa) {
-    alert("Golfinho");
-  } else if (pontuacaoContaGol === pontuacaoContaCoa) {
-    alert("igual");
+    let imgGol = document.createElement("img");
+    let imgTrofeu = document.createElement("img");
+    imgGol.classList.add("equipe", "img-add");
+    imgTrofeu.classList.add("equipe", "img-add");
 
+    imgGol.src = "./img/transferir.jpeg";
+    imgTrofeu.src = "./img/transferir (2).jpeg";
+
+    document.getElementById("trofeu").appendChild(imgGol);
+    document.getElementById("trofeu").appendChild(imgTrofeu);
+  } else if (pontuacaoContaGol === pontuacaoContaCoa) {
+    let imgGol = document.createElement("img");
+    let imgCoa = document.createElement("img");
+
+    imgGol.classList.add("equipe");
+    imgCoa.classList.add("equipe");
+
+    imgGol.src = "./img/transferir.jpeg";
+    imgCoa.src = "./img/transferir (1).jpeg";
+
+    document.getElementById("trofeu").appendChild(imgGol);
+    document.getElementById("trofeu").appendChild(imgCoa);
     console.log(pontuacaoContaGol + "\n" + pontuacaoContaCoa);
   } else {
-    alert("Coalas");
+    let imgGol = document.createElement("img");
+    let imgTrofeu = document.createElement("img");
+    imgGol.classList.add("equipe", "img-add");
+    imgTrofeu.classList.add("equipe", "img-add");
+
+    imgGol.src = "./img/transferir (1).jpeg";
+    imgTrofeu.src = "./img/transferir (2).jpeg";
+
+    document.getElementById("trofeu").appendChild(imgGol);
+    document.getElementById("trofeu").appendChild(imgTrofeu);
   }
+  document.getElementById("btn-media").disabled = true;
 }
 // }
